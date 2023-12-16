3352cc2d5c48468bfe710aef0df5fa9fa85189ee
Lucasshaw12 <shaw51@hotmail.co.uk>
2023-12-16 10:22:24 +0000

    allowed hosts and docker config

--- django_news_project/settings.py
+++ django_news_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '.azurewebsites.net']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '.azurewebsites.net', "0.0.0.0", "*"]
 
 
 # Application definition
